50f116d944869147eb8fda32e2fcd31c0217cbb7
mlodystary <107324279+JanuszkiewiczIgor@users.noreply.github.com>
2022-12-28 18:07:47 +0100

    Added fetching data.

---
+++ src/lib/CurrencyList.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+    import "./styles/country-list.css";
+    import type { IData } from "./interfaces/IData";
+    import Item from "./list/Item.svelte";
+
+    export let data: IData = {
+        table: "",
+        no: "",
+        effectiveDate: "",
+        rates: [
+            {
+                currency: "",
+                code: "",
+                mid: 0,
+            },
+        ],
+    };
+</script>
+
+<div>
+    {#each data.rates as rate}
+        <Item {rate} />
+    {/each}
+</div>
---
+++ src/lib/list/Item.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import "./item.css";
+    import type { IRate } from "../interfaces/IRate";
+
+    export let rate: IRate;
+</script>
+
+<div class="item">
+    <span class="currency">{JSON.stringify(rate["currency"])}</span>
+    <span class="code">{JSON.stringify(rate["code"])}</span>
+    <span class="mid">{JSON.stringify(rate["mid"])}</span>
+</div>
